882417c8cf682cddf157864fa777809314e67551
Park hanbaek <totwjfakd@gmail.com>
2023-04-04 11:30:48 +0000

    basic

--- openseats/__init__.py
+++ openseats/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask
-app = Flask(__name__)
 
-@app.route('/')
-def hello_pybo():
-    return 'Hello, Pybo!'
+
+def create_app() :
+    app = Flask(__name__)
+
+    from .views import main_views
+    app.register_blueprint(main_views.bp)
+
+    return app
