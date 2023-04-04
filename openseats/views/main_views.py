fd08efeef492ad9f10f612ce8e507d254b233276
Park hanbaek <totwjfakd@gmail.com>
2023-04-04 11:55:13 +0000

    add html

--- openseats/views/main_views.py
+++ openseats/views/main_views.py
@@ -1,7 +1,12 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 bp = Blueprint('main', __name__, url_prefix='/')
 
 @bp.route('/')
 def main_page() :
-    return "main"
+    return render_template('main.html')
+
+@bp.route('/group')
+def group_page() :
+    return render_template('group.html')
+    
