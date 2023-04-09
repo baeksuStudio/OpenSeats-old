78d528f224466b4eed060dd6e8cb9701ff8bc19e
Insu Oh <ohinsu134@gmail.com>
2023-04-09 13:28:06 +0000

    try to merge

--- openseats/__init__.py
+++ openseats/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 
 
 def create_app() :
-    app = Flask(__name__, template_folder='templates')
+    app = Flask(__name__)
 
     from .views import main_views
     app.register_blueprint(main_views.bp)
